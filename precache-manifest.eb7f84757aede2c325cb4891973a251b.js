a7c7165dc3128805564612f65580932c98d7d434
iamishuchawla <chawlaishu1@gmail.com>
2021-09-05 20:11:29 +0530

    Updates

---
+++ precache-manifest.eb7f84757aede2c325cb4891973a251b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23364e55712b4e6ae390da3bab87552",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "2ebac5cca21043e745f8",
+    "url": "/react-todo-list/static/css/main.5efcb2cd.chunk.css"
+  },
+  {
+    "revision": "8f7c055f1f7d47a70494",
+    "url": "/react-todo-list/static/js/2.ee5a4b7b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-todo-list/static/js/2.ee5a4b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebac5cca21043e745f8",
+    "url": "/react-todo-list/static/js/main.8e6d58c5.chunk.js"
+  },
+  {
+    "revision": "91a55604ac59526b5881",
+    "url": "/react-todo-list/static/js/runtime-main.a072b4a7.js"
+  },
+  {
+    "revision": "7832cf73e3be83f87350a45deeb0a08c",
+    "url": "/react-todo-list/static/media/trash.7832cf73.svg"
+  }
+]);
